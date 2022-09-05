d984cc0815d78e02e0928ee0d6851c0446c8b037
namton12 <namton12102000@gmail.com>
2022-09-05 09:04:36 +0700

    Appbar and BoardBar change

--- src/Components/Appbar/Appbar.jsx
+++ src/Components/Appbar/Appbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './Appbar.scss'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BiInfoCircle, BiLogOutCircle, BiSearch } from 'react-icons/bi'
+import { BsGrid3X3GapFill } from 'react-icons/bs'
+import { FaRegBell, FaToriiGate } from 'react-icons/fa'
+import { VscHome } from 'react-icons/vsc'
 export default function Appbar() {
   return (
     <div>
-        <nav className='navbar-app'> App Bar </nav>
+      <nav className="navbar-app">
+        <div className="navbar-left">
+          <button className="navbar-btn">
+            <BsGrid3X3GapFill />
+          </button>
+          <button className="navbar-btn">
+            <VscHome />
+          </button>
+          <button className="navbar-btn">
+            <FaToriiGate />
+            <span className="navbar-btn-text">Boards</span>
+          </button>
+          <div className="navbar-input">
+            <input type="text" placeholder="Jump to..." />
+            <span className="navbar-btn">
+              <BiSearch />
+            </span>
+          </div>
+        </div>
+        <div className="navbar-right">
+          <button className="navbar-btn">
+            <AiOutlinePlus />
+          </button>
+          <button className="navbar-btn">
+            <FaRegBell />
+          </button>
+          <button className="navbar-btn">
+            <BiInfoCircle />
+          </button>
+          <img
+            src="https://avatars.githubusercontent.com/thairyo"
+            className="circle-image"
+          />
+          <button className="navbar-btn btn-red">
+            <BiLogOutCircle />
+            <span className="navbar-btn-text">Logout</span>
+          </button>
+        </div>
+      </nav>
     </div>
   )
 }
--- src/Components/BoardBar/BoardBar.jsx
+++ src/Components/BoardBar/BoardBar.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 import './BoardBar.scss'
+import { BiStar } from 'react-icons/bi'
+import { FaToriiGate } from 'react-icons/fa'
+import { MdKeyboardArrowDown, MdMoreHoriz, MdPublic } from 'react-icons/md'
+import { RiChatPrivateFill } from 'react-icons/ri'
 export default function BoardBar() {
   return (
     <div>
-      <nav className='navbar-board'>Board Bar</nav>
+      <nav className="board-app">
+        <div className="boardbar-left">
+          <button className="boardbar-btn">
+            <FaToriiGate />
+            <span className="boardbar-btn-text">Nam Tôn</span>
+            <MdKeyboardArrowDown />
+          </button>
+          <div className="cut-btn"></div>
+          <button className="boardbar-btn">#fullstack-trello-clone</button>
+          <button className="boardbar-btn">
+            <BiStar />
+          </button>
+          <div className="cut-btn"></div>
+          <button className="boardbar-btn">
+            <MdPublic />
+            <span className="boardbar-btn-text">Public</span>
+          </button>
+          <button className="boardbar-btn">
+            <RiChatPrivateFill />
+            <span className="boardbar-btn-text">Private workspace</span>
+          </button>
+          <div className="cut-btn"></div>
+          <button className="boardbar-btn">
+            <FaToriiGate />
+            <span className="boardbar-btn-text">Automation</span>
+          </button>
+          <div className="cut-btn"></div>
+          <div className="boardbar-list-image">
+            <img
+              src="https://avatars.githubusercontent.com/thairyo"
+              className="circle-image"
+            />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+              className="circle-image"
+            />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+              className="circle-image"
+            />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+              className="circle-image"
+            />
+            <button className="circle-btn">+7</button>
+          </div>
+          <button className="boardbar-btn">Invite</button>
+        </div>
+        <div className="boardbar-right">
+          <button className="boardbar-btn">
+            <MdMoreHoriz />
+            <span className="boardbar-btn-text">Show Menu</span>
+          </button>
+        </div>
+      </nav>
     </div>
   )
 }
